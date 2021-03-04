2e9786f782762ce72c44000c818239ad0a03a236
popoa1000 <gecko.ekoda@gmail.com>
2021-03-04 12:18:01 +0900

    ユーザーモデルwith_options実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,16 +4,24 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         validates :nickname,           presence: true
-         PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-         validates_format_of :password, with: PASSWORD_REGEX,message: 'Include both letters and numbers' 
-         validates :first_name,         presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々]/, message: "is invalid. Input full-width characters." }
-         validates :last_name,          presence: true,  format: { with: /\A[ぁ-んァ-ン一-龥々]/, message: "is invalid. Input full-width characters." }
-         validates :first_name_kana,    presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters." } 
-         validates :last_name_kana,     presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters." }
-         validates :birthday,           presence: true
-       
-         has_many :items
-         has_many :comments
-         has_many :buys
+        with_options presence: true do
+          validates :nickname
+          PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+          validates_format_of :password, with: PASSWORD_REGEX,message: 'Include both letters and numbers' 
+          with_options format: { with: /\A[ぁ-んァ-ン一-龥々]/, message: "is invalid. Input full-width characters." } do
+            validates :first_name
+            validates :last_name
+          end
+          with_options format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters." } do
+            validates :first_name_kana
+            validates :last_name_kana
+          end
+          with_options numericality: { with: /\A\d{4}-\d{2}-\d{2}\z/ } do
+            validates :birthday
+          end
+        end
+
+        has_many :items
+        has_many :comments
+        has_many :buys
 end
