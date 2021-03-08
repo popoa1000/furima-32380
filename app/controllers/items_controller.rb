8f5dee3ae3d14cd77b7f0305c759ee7aa4d5c011
popoa1000 <gecko.ekoda@gmail.com>
2021-03-09 00:31:45 +0900

    金額設定欄の設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,6 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!,only: [:new]
+
   def index
     @items = Item.all
   end
@@ -8,12 +10,18 @@ class ItemsController < ApplicationController
   end
 
   def create
-    Item.create(item_params)
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
   def item_params
-    params.require(:item).permit(:description, :category_id, :condition_id,
+    params.require(:item).permit(:product, :description, :category_id, :condition_id,
       :charge_id, :shipment_id, :day_id, :price, :user, :image).merge(user_id: current_user.id)
-    end
+  end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,16 +1,20 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
+  
+
+  with_options presence: true do
+    validates :product, :description
+    validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
+    with_options numericality: { other_than: 1 }do
+      validates :category_id, :condition_id, :charge_id, :shipment_id, :day_id
+    end
+  end
+
   belongs_to :category
   belongs_to :condition
   belongs_to :charge
   belongs_to :shipment
   belongs_to :day
-
-  validates :product, :description, presence: true
-
-  with_options numericality: { other_than: 1 }do
-    validates :category, :condition, :charge, :shipment, :day
-  end
 
   belongs_to :user
   has_many :comments
