f067c3febb3b4b13b070495b824e5affc0123528
popoa1000 <gecko.ekoda@gmail.com>
2021-03-14 23:27:13 +0900

    ルーティングとコントローラーの編集

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   get 'items/index'
   root to: "items#index"
   resources :users, only: [:new, :create]
-  resources :items, only: [:index, :new, :create, :show, :edit, :update]
+  resources :items, only: [:index, :new, :create, :show, :edit, :update, :destroy]
 
 end
