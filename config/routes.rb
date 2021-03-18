0f1fd605bb6304737bc402f0bd05a7de98388b37
popoa1000 <gecko.ekoda@gmail.com>
2021-03-18 14:14:56 +0900

    クレジット機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   root to: "items#index"
   resources :users, only: [:new, :create]
   resources :items, only: [:index, :new, :create, :show, :edit, :update, :destroy]
-
+  resources :orders, only:[:index, :new, :create]
 end
