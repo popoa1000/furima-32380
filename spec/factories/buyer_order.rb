73ab17ec37010ba6bf09d22f637ad1822283a11c
popoa1000 <gecko.ekoda@gmail.com>
2021-03-30 23:32:26 +0900

    テストコードの実装

---
+++ spec/factories/buyer_order.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :buyer_order do
+    postal_code { '012-3456' }
+    shipment_id { Faker::Number.between(from: 2, to: 11) }
+    municipality { Faker::Address.city }
+    address { Faker::Address.community }
+    building { Faker::Lorem.sentence }
+    phone_number { Faker::Number.leading_zero_number(digits: 10||11)}
+    token { Faker::Code.nric }
+    user_id {Faker::Number.non_zero_digit}
+    item_id {Faker::Number.non_zero_digit}
+  end
+end
---
+++ spec/models/buyer_order_spec.rb
@@ -0,0 +1,103 @@
+require 'rails_helper'
+
+RSpec.describe BuyerOrder, type: :model do
+  describe '購入情報の保存' do
+    before do
+      @buyer_order = FactoryBot.build(:buyer_order)
+    end
+
+    context '購入がうまくいくとき' do
+      it 'postal_code, shipment_id, municipality, address, building, phone_number,
+          user_id, item_id, tokenが存在すれば購入できる' do
+        expect(@buyer_order).to be_valid
+      end
+      it '建物名がなくても購入できる' do
+        @buyer_order.building = ''
+        expect(@buyer_order).to be_valid
+      end
+    end
+  
+    context '購入がうまくいかないとき' do
+      it '郵便番号が空では購入できない' do
+        @buyer_order.postal_code = ''
+        @buyer_order.valid?
+        expect(@buyer_order.errors.full_messages).to include("Postal code can't be blank")
+      end
+      it '郵便番号にハイフンがないと購入できない' do
+        @buyer_order.postal_code = '1234567'
+        @buyer_order.valid?
+        expect(@buyer_order.errors.full_messages).to include("Postal code is invalid")
+      end
+      it '郵便番号が全角数字だと購入できない' do
+        @buyer_order.postal_code = '１２３４５６７'
+        @buyer_order.valid?
+        expect(@buyer_order.errors.full_messages).to include("Postal code is invalid")
+      end
+      it '郵便番号が半角英数字混合だと購入できない' do
+        @buyer_order.postal_code = 'l2345b7'
+        @buyer_order.valid?
+        expect(@buyer_order.errors.full_messages).to include("Postal code is invalid")
+      end
+      it '郵便番号が半角英字だと購入できない' do
+        @buyer_order.postal_code = 'OlZbqSo'
+        @buyer_order.valid?
+        expect(@buyer_order.errors.full_messages).to include("Postal code is invalid")
+      end
+      it '都道府県がないと購入できない' do
+        @buyer_order.shipment_id = ''
+        @buyer_order.valid?
+        expect(@buyer_order.errors.full_messages).to include("Shipment can't be blank")
+      end
+      it '市区町村がないと購入できない' do
+        @buyer_order.municipality = ''
+        @buyer_order.valid?
+        expect(@buyer_order.errors.full_messages).to include("Municipality can't be blank")
+      end
+      it '番地がないと購入できない' do
+        @buyer_order.address = ''
+        @buyer_order.valid?
+        expect(@buyer_order.errors.full_messages).to include("Address can't be blank")
+      end
+      it '電話番号がないと購入できない' do
+        @buyer_order.phone_number = ''
+        @buyer_order.valid?
+        expect(@buyer_order.errors.full_messages).to include("Phone number can't be blank")
+      end
+      it '電話番号にハイフンがあると購入できない' do
+        @buyer_order.phone_number = '090-1234-5678'
+        @buyer_order.valid?
+        expect(@buyer_order.errors.full_messages).to include("Phone number is invalid")
+      end
+      it '電話番号が全角数字だと購入できない' do
+        @buyer_order.phone_number = '０１２３４５６７８９'
+        @buyer_order.valid?
+        expect(@buyer_order.errors.full_messages).to include("Phone number is invalid")
+      end
+      it '電話番号が半角英数字混合では購入できない' do
+        @buyer_order.phone_number = 'ol2345678a'
+        @buyer_order.valid?
+        expect(@buyer_order.errors.full_messages).to include("Phone number is invalid")
+      end
+      it '電話番号が半角英語では購入できない' do
+        @buyer_order.phone_number = 'oqolllloqbb'
+        @buyer_order.valid?
+        expect(@buyer_order.errors.full_messages).to include("Phone number is invalid")
+      end
+      it '電話番号が9桁以下では購入できない' do
+        @buyer_order.phone_number = '090123456'
+        @buyer_order.valid?
+        expect(@buyer_order.errors.full_messages).to include("Phone number is invalid")
+      end
+      it '電話番号が12桁以上では購入できない' do
+        @buyer_order.phone_number = '090123456789'
+        @buyer_order.valid?
+        expect(@buyer_order.errors.full_messages).to include("Phone number is invalid")
+      end
+      it 'クレジット情報のtokenがないと購入できない' do
+        @buyer_order.token = ''
+        @buyer_order.valid?
+        expect(@buyer_order.errors.full_messages).to include("Token can't be blank")
+      end
+    end
+  end
+end
