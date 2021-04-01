8d718b911ab5afbe97c04068dbb3dd5641b9b7a2
popoa1000 <gecko.ekoda@gmail.com>
2021-04-01 21:59:08 +0900

    routes修正と変更

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get 'items/index'
   root to: "items#index"
   resources :users, only: [:new, :create]
-  resources :items, only: [:index, :new, :create, :show, :edit, :update, :destroy] do
+  resources :items do
     resources :orders, only: [:index, :new, :create]
   end
 
