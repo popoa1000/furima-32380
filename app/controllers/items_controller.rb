55c396d09db0a6e015f3e76c666fdd6baba93209
popoa1000 <gecko.ekoda@gmail.com>
2021-04-02 14:32:46 +0900

    コントローラーmove_to_indexを１行にまとめた

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -51,8 +51,7 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    redirect_to action: :index unless current_user.id == @item.user_id
-    redirect_to action: :index if @item.order.present?
+    redirect_to action: :index if current_user.id =! @item.user_id || @item.order.present?
   end
 
 end
