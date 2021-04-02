edbd9cbb414577d004abe03be5b6998e11597e3a
popoa1000 <gecko.ekoda@gmail.com>
2021-04-02 14:42:42 +0900

    ルーティング内の整理

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
-  get 'orders/index'
   devise_for :users
   get 'items/index'
   root to: "items#index"
   resources :users, only: [:new, :create]
   resources :items do
-    resources :orders, only: [:index, :new, :create]
+    resources :orders, only: [:index, :create]
   end
 
 end
